77aa8004fd764ed1c89d95637b1c415c5870571b
André Castro <dedecastrobr@gmail.com>
2025-02-21 23:05:01 +0000

    Light patterns.

--- seubot/seubot.py
+++ seubot/seubot.py
@@ -8,6 +8,7 @@ from commands import Command, CommandState, CommandManager
 from admin import start_web_server
 from utils import get_config, Logger
 
+
 config = get_config()
 logger = Logger(config.get("bot_logfile"))
 
@@ -22,17 +23,16 @@ class SeuBot:
 
         self.motor_set = MotorSet(self.gamepad)
 
-        self.green_light = Light("green_light")
-        self.yellow_light = Light("yellow_light")
         self.white_light = Light("white_light")
         self.blue_light = Light("blue_light")
+        self.green_light = Light("green_light")
+        self.yellow_light = Light("yellow_light")
 
         self.command_manager = CommandManager(self.get_available_commands())
         self.command_handlers = {
             CommandState.QUIT: self.quit,
-            CommandState.PRESSED_A: self.light_show_pattern_one,
-            CommandState.PRESSED_B: self.light_show_pattern_two,
-            CommandState.PRESSED_X: self.stop_ligths
+            CommandState.PRESSED_A: self.light_show_wave,
+            CommandState.PRESSED_X: self.light_show_dance
         }
 
         if config.get("enable_admin"):
@@ -41,13 +41,9 @@ class SeuBot:
     def what_todo(self):
         if self.gamepad.is_connected():
             self.motor_set.handle_commands()
-            self.green_light.handle_commands()
             active_commands = self.command_manager.get_active_commands()
-            self.yellow_light.on()
             for command_state in active_commands:
                 self.command_handlers[command_state]()
-        else:
-            self.yellow_light.off()
 
     def get_available_commands(self):
         return [
@@ -64,20 +60,8 @@ class SeuBot:
                 )
             ),
             Command(
-                state=CommandState.PRESSED_B,
-                condition=lambda button=self.gamepad.get_buttons().B: (
-                    self.gamepad.getButtonState(int(button)) == 1
-                )
-            ),
-            Command(
                 state=CommandState.PRESSED_X,
                 condition=lambda button=self.gamepad.get_buttons().X: (
-                    self.gamepad.getButtonState(int(button)) == 1
-                )
-            ),
-            Command(
-                state=CommandState.PRESSED_Y,
-                condition=lambda button=self.gamepad.get_buttons().Y: (
                     self.gamepad.getButtonState(int(button)) == 1
                 )
             )
@@ -92,32 +76,67 @@ class SeuBot:
         logger.info("Restarting "+ self.name)
         os.execv(sys.executable, ['python'] + sys.argv)
 
-    def light_show_pattern_one(self):
+    def light_show_wave(self):
+
         for _ in range(3):
+            # Forward wave
+            self.white_light.on()
+            sleep(0.3)
+            self.blue_light.on()
+            sleep(0.3)
             self.green_light.on()
-            sleep(0.5)
-            self.green_light.off()
+            sleep(0.3)
             self.yellow_light.on()
             sleep(0.5)
+            
+            # Backward wave
             self.yellow_light.off()
-            self.white_light.on()
-            sleep(0.5)
-            self.white_light.off()
-            self.blue_light.on()
-            sleep(0.5)
+            sleep(0.3)
+            self.green_light.off()
+            sleep(0.3)
             self.blue_light.off()
-
-    def light_show_pattern_two(self):
-        for _ in range(3):
-            self.green_light.blink()
-            self.yellow_light.blink()
-            sleep(0.5)
-            self.white_light.blink()
-            self.blue_light.blink()
+            sleep(0.3)
+            self.white_light.off()
             sleep(0.5)
 
-    def stop_ligths(self):
-        self.green_light.off()
-        self.yellow_light.off()
-        self.white_light.off()
-        self.blue_light.off()
+    def light_show_dance(self):
+
+        # Phase 1: Alternating pairs
+        for _ in range(5):
+            self.white_light.on()
+            self.green_light.on()
+            self.blue_light.off()
+            self.yellow_light.off()
+            sleep(0.2)
+            
+            self.white_light.off()
+            self.green_light.off()
+            self.blue_light.on()
+            self.yellow_light.on()
+            sleep(0.2)
+        
+        # Phase 2: All lights blink together
+        all_lights = [self.white_light, self.blue_light, 
+                    self.green_light, self.yellow_light]
+        
+        for light in all_lights:
+            light.off()
+        
+        for _ in range(4):
+            for light in all_lights:
+                light.toggle()
+            sleep(0.15)
+        
+        # Phase 3: Sequential blinking
+        for light in all_lights:
+            light.off()
+        
+        for light in all_lights:
+            light.blink()
+            sleep(0.3)
+        
+        sleep(1)
+        
+        # Turn everything off at the end
+        for light in all_lights:
+            light.off()
